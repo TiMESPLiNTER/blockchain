2d933ff6c536b6750f86210a0d131272414e119b
TiMESPLiNTER <github@timesplinter.ch>
2018-02-02 14:08:07 +0100

    Signed transactions

---
+++ src/BlockchainIterator.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Timesplinter\Blockchain;
+
+
+class BlockchainIterator
+{
+
+}
---
+++ src/Transaction/TransactionBlockchain.php
@@ -0,0 +1,154 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Timesplinter\Blockchain\Transaction;
+
+use Timesplinter\Blockchain\BlockchainInterface;
+use Timesplinter\Blockchain\BlockInterface;
+use Traversable;
+
+final class TransactionPool implements BlockchainInterface
+{
+
+    /**
+     * @var BlockchainInterface
+     */
+    private $blockchain;
+
+    /**
+     * @var array
+     */
+    private $pool = [];
+
+    /**
+     * @param BlockchainInterface $blockchain
+     */
+    public function __construct(BlockchainInterface $blockchain)
+    {
+        $this->blockchain = $blockchain;
+    }
+
+    /**
+     * @param Transaction $transaction
+     * @return bool
+     */
+    public function addTransaction(Transaction $transaction): bool
+    {
+        if (false === $this->isTransactionValid($transaction)) {
+            return false;
+        }
+
+        $this->pool[] = $transaction;
+
+        return true;
+    }
+
+    /**
+     * @param Transaction $transaction
+     * @return bool
+     */
+    public function isTransactionValid(Transaction $transaction): bool
+    {
+        $balanceOfSender = $this->getBalanceForAddress($transaction->getFrom());
+
+        return $balanceOfSender >= $transaction->getAmount();
+    }
+
+    /**
+     * @param string $address
+     * @return float
+     */
+    public function getBalanceForAddress(string $address): float
+    {
+        $balance = 0.0;
+
+        foreach ($this->blockchain as $block) {
+            if (false === is_array($block->getData())) {
+                continue;
+            }
+
+            foreach ($block->getData() as $transaction) {
+                if (false === $transaction instanceof Transaction) {
+                    continue;
+                }
+
+                /** @var Transaction $transaction */
+
+                if ($address === $transaction->getFrom()) {
+                    $balance -= $transaction->getAmount();
+                } elseif ($address === $transaction->getTo()) {
+                    $balance += $transaction->getAmount();
+                }
+            }
+        }
+
+        return $balance;
+    }
+
+    /**
+     * Adds new block to the chain
+     * @param BlockInterface $block
+     * @return void
+     */
+    public function addBlock(BlockInterface $block): void
+    {
+        if (false === $this->isBlockDataValid($block)) {
+            throw new \RuntimeException(
+                sprintf('Data of block "%s" is not valid', $block->getHash())
+            );
+        }
+
+        $this->blockchain->addBlock($block);
+    }
+
+    /**
+     * Returns latest block of the chain
+     * @return BlockInterface
+     */
+    public function getLatestBlock(): BlockInterface
+    {
+        return $this->blockchain->getLatestBlock();
+    }
+
+    /**
+     * Checks if the blockchain is in a valid state
+     * @return bool
+     */
+    public function isValid(): bool
+    {
+        return $this->blockchain->isValid();
+    }
+
+    /**
+     * Checks if the block only contains valid transaction objects
+     * @param BlockInterface $block
+     * @return bool
+     */
+    private function isBlockDataValid(BlockInterface $block): bool
+    {
+        if (false === is_array($block->getData())) {
+            return false;
+        }
+
+        foreach ($block->getData() as $transaction) {
+            if (false === $transaction instanceof Transaction) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Retrieve an external iterator
+     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
+     * @return Traversable An instance of an object implementing <b>Iterator</b> or
+     * <b>Traversable</b>
+     * @since 5.0.0
+     */
+    public function getIterator()
+    {
+        return $this->blockchain->getIterator();
+    }
+}
